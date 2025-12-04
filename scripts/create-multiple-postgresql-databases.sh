f48544946c6536e01458f8bea16f0e512293fc44
Cristianv222 <cris.vasq2003@hotmail.com>
2025-12-04 00:09:08 -0500

    cambio para que funcione

--- scripts/create-multiple-postgresql-databases.sh
+++ scripts/create-multiple-postgresql-databases.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+
 set -e
 set -u
 
 function create_database() {
     local database=$1
-    echo "Creating database '$database'"
+    echo "  Creating database '$database'"
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-        CREATE DATABASE $database;
+        SELECT 'CREATE DATABASE $database'
+        WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$database')\gexec
         GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
 EOSQL
 }
@@ -16,5 +18,5 @@ if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
     for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
         create_database $db
     done
-    echo "Multiple databases created"
+    echo "Multiple databases created successfully"
 fi
